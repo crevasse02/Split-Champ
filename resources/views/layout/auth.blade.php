17dcbb8111d795fa035d905d7281b32ea261b6d2
crevasse02 <wiboworaffi82@gmail.com>
2024-11-01 16:38:37 +0700

    remove some library

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -20,7 +20,6 @@
         rel="stylesheet">
 
     {{-- Jquery --}}
-
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
@@ -28,10 +27,10 @@
     <link href="{{ URL::asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    {{-- <link href="{{ URL::asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet"> --}}
 
     {{-- Sweet Alert --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.14.4/dist/sweetalert2.all.min.js"></script>
@@ -40,13 +39,6 @@
     <!-- Template Main CSS File -->
     <link href="{{ URL::asset('assets/css/style.css') }}" rel="stylesheet">
 
-    <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
@@ -60,14 +52,14 @@
             class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="{{ URL::asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    {{-- <script src="{{ URL::asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script> --}}
     <script src="{{ URL::asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
+    {{-- <script src="{{ URL::asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
     <script src="{{ URL::asset('assets/vendor/echarts/echarts.min.js') }}"></script>
     <script src="{{ URL::asset('assets/vendor/quill/quill.js') }}"></script>
     <script src="{{ URL::asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
     <script src="{{ URL::asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ URL::asset('assets/vendor/php-email-form/validate.js') }}"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- Template Main JS File -->
