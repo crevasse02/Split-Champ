947ad0fe2d302851eb1f0f8f8a7de82982b11331
crevasse02 <wiboworaffi82@gmail.com>
2024-11-04 15:06:21 +0700

    up

--- app/Http/Controllers/experimentController.php
+++ app/Http/Controllers/experimentController.php
@@ -17,9 +17,8 @@ class experimentController extends Controller
 
     public function store(Request $request)
     {
-        // dd($request->all());
         try {
-            // Validasi input
+            // Validate input
             $validated = $request->validate([
                 'eksperimen_name' => 'required|string|max:255',
                 'domain_name' => 'required|string|max:255',
@@ -29,17 +28,21 @@ class experimentController extends Controller
                 'domain_name.required' => 'Nama domain wajib diisi.',
                 'created_by.required' => 'Nama pembuat wajib diisi.',
             ]);
-
-            // Simpan data ke database
+    
+            // Trim 'domain_name' to remove 'https://', 'www', and trailing slashes
+            $validated['domain_name'] = rtrim(ltrim(preg_replace('/^https?:\/\/(www\.)?/', '', $validated['domain_name']), '/'), '/');
+    
+            // Save data to database
             ExperimentModel::create($validated);
-
+    
             return response()->json(['message' => "Experimen created successfully", "code" => "200"], 201);
         } catch (\Illuminate\Validation\ValidationException $e) {
-            // Mengembalikan respons dengan pesan kesalahan
+            // Return response with error messages
             return response()->json([
                 'message' => 'Validasi gagal',
                 'errors' => $e->validator->errors(),
             ], 422); // 422 Unprocessable Entity
         }
     }
+    
 }
