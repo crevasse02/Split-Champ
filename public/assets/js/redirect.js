0b5fedf5a8223f752a9f45b4b193ef98cc698b4d
crevasse02 <wiboworaffi82@gmail.com>
2024-11-04 09:55:51 +0700

    update redirect to latest

--- public/assets/js/redirect.js
+++ public/assets/js/redirect.js
@@ -1,7 +1,7 @@
 const redirectScript = `
   (function () {
     const script = document.createElement("script");
-    script.src = "https://cdn.jsdelivr.net/gh/crevasse02/testing-js@main/redirect2.js";
+    script.src = "https://cdn.jsdelivr.net/gh/crevasse02/testing-js@latest/redirect2.js";
     script.async = true;
     document.head.appendChild(script);
 })()
